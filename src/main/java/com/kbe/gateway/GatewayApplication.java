abc64b0d9297080ca9daa07071a11079451cf1a5
ChristianFuchs <chris.fuchs88@gmail.com>
2022-09-21 20:29:51 +0200

    KBES22-9 keycloak settings and basic rest calls

--- src/main/java/com/kbe/gateway/GatewayApplication.java
+++ src/main/java/com/kbe/gateway/GatewayApplication.java
@@ -2,12 +2,25 @@ package com.kbe.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/anonymous").allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
--- src/main/java/com/kbe/gateway/controller/ProductController.java
+++ src/main/java/com/kbe/gateway/controller/ProductController.java
@@ -1,16 +1,43 @@
 package com.kbe.gateway.controller;
 
 import com.kbe.gateway.rabbitmq.RabbitmqSender;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProductController {
 
-    @GetMapping("/products")
-    String getAllProducts() {
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/hardwarecomponents")
+    public ResponseEntity<String> getAllProducts() {
         System.out.println("products requested");
-        new RabbitmqSender().send("products");
-        return "all";
+        //new RabbitmqSender().send("products");
+        String json = "{\"id\":0,\"name\":\"wunder\",\"hardware\":[{\"id\":0,\"type\":\"gpu\",\"name\":\"rx 6800\",\"description\":\"fast\",\"price\":222.42,\"stock\":5}";
+        return ResponseEntity.ok(json);
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @RequestMapping(value = "/anonymous", method = RequestMethod.GET)
+    public ResponseEntity<String> getAnonymous() {
+        //ResponseEntity<String> response = ResponseEntity.ok("Hello Anonymous");
+        //System.out.println("response.toString(): "+response);
+        System.out.println("getAnonymous triggered");
+        return ResponseEntity.ok("Hello Anonymous");
+        //return response;
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public ResponseEntity<String> getUser() {
+        return ResponseEntity.ok("Hello User");
+    }
+
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public ResponseEntity<String> getAdmin() {
+        return ResponseEntity.ok("Hello Admin");
+    }
+
+    @RequestMapping(value = "/all-user", method = RequestMethod.GET)
+    public ResponseEntity<String> getAllUser() {
+        return ResponseEntity.ok("Hello All User");
     }
 }
